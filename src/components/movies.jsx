1d246333d69c381e5a844d05e633c0d67e58f6e9
Pista Academy <pista.academy@gmail.com>
2023-04-10 15:22:27 +0430

    Session-13

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { getMovies } from '../services/fakeMovieService';
 
+import Like from './like';
+
 class Movies extends Component {
     state = {
         movies: getMovies()
@@ -10,6 +12,14 @@ class Movies extends Component {
     handleDelete = (movie) => {
         const movies = this.state.movies.filter(m => m._id != movie._id)
         this.setState({ movies})
+    }
+
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]}
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies })
     }
 
     render() {
@@ -29,6 +39,7 @@ class Movies extends Component {
                         <th>Stock</th>
                         <th>Rate</th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -38,7 +49,13 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
-                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                            <td><Like liked={movie.liked} onLike={() => this.handleLike(movie)} /></td>
+                            <td>
+                                <button 
+                                    onClick={() => this.handleDelete(movie)} 
+                                    className="btn btn-danger btn-sm"
+                                >Delete
+                                </button></td>
                         </tr>
                     ))}
                 </tbody>
