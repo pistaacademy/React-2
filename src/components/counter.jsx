306444e3840588731b4ed67abf543add96204488
Pista Academy <pista.academy@gmail.com>
2023-04-10 11:05:03 +0430

    session-4

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,22 +4,41 @@ class Counter extends Component {
 
     state = {
         count: 0,
-        names: []
     };
 
-    renderTags() {
-        if (this.state.names.length === 0) return <p>There are no tags!</p>;
-        return <ul>{this.state.names.map(name => <li key={name}>{name}</li>)}</ul>
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 })
     }
+
 
     render() {
         return (
             <React.Fragment>
-                {this.state.names.length === 0 && "There are no tags!"}
-                {this.renderTags()}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    className='btn btn-secondary btn-sm'
+                    onClick={this.handleIncrement}
+                >
+                    Increment
+                </button>
             </React.Fragment>
         )
     };
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes
+    }
+
+    formatCount() {
+        return this.state.count === 0 ? 'Zero' : this.state.count;
+    }
 
 }
 
